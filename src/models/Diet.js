ad86cbfb70cc4a4af4a6b3055eb9832ce6d6ca5b
EEErban <f.erban@outlook.com>
2021-06-12 14:56:37 +0100

    Diet model initial file

---
+++ src/models/Diet.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Diet extends Model {}
+
+const schema = {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+  },
+   name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+  },
+
+  description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+  }
+        
+}
+
+const options = {
+    sequelize,
+    modelName: "diet",
+    timestamps: true,
+    underscored: true,
+    freezeTableName: true,
+  };
+
+
+Diet.init(schema, options);
+
+module.exports = Diet;
