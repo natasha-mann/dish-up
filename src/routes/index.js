6b0534c9c41a14147b989a6454192d4badd345d3
EEErban <f.erban@outlook.com>
2021-06-15 21:16:44 +0100

    Initial api routes created

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 
 const htmlRoutes = require("./html");
+const apiRoutes = require("./api");
 
 const router = Router();
 
-// router.use("/api", apiRoutes);
+router.use("/api", apiRoutes);
 // router.use("/auth", authRoutes);
 
 router.use("/", htmlRoutes);
