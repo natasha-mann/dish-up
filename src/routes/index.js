7f9c61cba144b6d338e5d1263401f02da6ac06cd
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-06-15 20:58:37 +0100

    added auth routes and controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 
+const authRoutes = require("./auth");
 const htmlRoutes = require("./html");
 
 const router = Router();
 
 // router.use("/api", apiRoutes);
-// router.use("/auth", authRoutes);
 
+router.use("/auth", authRoutes);
 router.use("/", htmlRoutes);
 
 module.exports = router;
