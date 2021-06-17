164ed06a38ca0aadb8117be4c793d911e73071b3
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-06-17 21:14:15 +0100

    added get all meal plans controller

--- src/controllers/api/mealPlans.js
+++ src/controllers/api/mealPlans.js
@@ -1,6 +1,27 @@
-const getAllMealPlans = (req, res) => {
-res.status(200).json({message: "get All mealplans"})
+const { MealPlan, Day, Meal } = require("../../models");
 
+const getAllMealPlans = async (req, res) => {
+  try {
+    const mealPlanData = await MealPlan.findAll({
+      include: {
+        model: Day,
+        include: [
+          { model: Meal, as: "breakfast" },
+          { model: Meal, as: "lunch" },
+          { model: Meal, as: "dinner" },
+        ],
+      },
+    });
+
+    const mealPlans = mealPlanData.map((mealPlan) =>
+      mealPlan.get({ plain: true })
+    );
+
+    return res.status(200).json(mealPlans);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: "Failed to get all meal plans." });
+  }
 };
 
-module.exports = {getAllMealPlans};
+module.exports = { getAllMealPlans };
