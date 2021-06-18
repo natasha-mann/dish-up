13ddabd112edb47bd635f3f5df84441c60b97a7d
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-06-18 19:07:16 +0100

    login form client-side handle submit

---
+++ public/assets/js/login.js
@@ -0,0 +1,29 @@
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const email = $("#email").val();
+  const password = $("#password").val();
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    body: JSON.stringify(email, password),
+  };
+  const response = await fetch("/auth/login", options);
+  if (response.status !== 200) {
+    console.error("Failed login");
+    $("#alert-div").empty();
+    $("#alert-div")
+      .append(`<div id="error-alert" class="alert alert-warning d-flex align-items-center" role="alert">
+    <i class="fas fa-exclamation-triangle me-4"></i>
+    <div>
+      Email or password incorrect. Please try again!
+    </div>
+  </div>`);
+  } else {
+    window.location.replace("/dashboard");
+  }
+};
+
+$("#login-form").submit(handleSubmit);
