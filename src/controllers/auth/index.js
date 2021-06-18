1fc33c7256ff3f3ef05d060318a3c93a77619833
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-18 19:18:42 +0100

    added password hashing

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,4 +1,8 @@
+const { User } = require("../../models");
+
 const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ where: { email } });
   res.send("login");
 };
 
