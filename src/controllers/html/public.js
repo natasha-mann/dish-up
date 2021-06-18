a06a423312df6cd543771b49b0f7a8eae487295d
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-18 23:01:55 +0100

    added check on homepage to see if loggedin

--- src/controllers/html/public.js
+++ src/controllers/html/public.js
@@ -1,9 +1,22 @@
+const { Meal } = require("../../models");
+
 const renderLandingPage = (req, res) => {
   res.render("landing-page");
 };
 
-const renderHomePage = (req, res) => {
-  res.render("homepage");
+const renderHomePage = async (req, res) => {
+  try {
+    const { isLoggedIn } = req.session;
+    const mealData = await Meal.findAll();
+    const meals = mealData.map((meal) => {
+      return meal.get({ plain: true });
+    });
+
+    res.render("homepage", { isLoggedIn });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "Failed to render" });
+  }
 };
 
 const renderLogin = (req, res) => {
