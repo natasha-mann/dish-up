eff91e7ed65ebfc74b5395da3bc158fcaea325fe
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-06-19 17:19:01 +0100

    get data from input fields

--- public/assets/js/add-meal.js
+++ public/assets/js/add-meal.js
@@ -4,18 +4,25 @@ const handleSubmit = (event) => {
   const meal = $("#search-title").attr("data-meal");
   const day = $("#search-title").attr("data-day");
   const id = $("#search-title").attr("data-id");
-  // const searchInput = $("#search-input").val();
 
-  // const diet = $("#diet-input")
-  //   .find(":checked")
-  //   .map((each) => {
-  //     return $(each).attr("id");
-  //   });
-  // console.log(diet);
+  const searchInput = $("#search-input").val();
+  const diet = $("#diet-input")
+    .find(":checked")
+    .map((i, each) => {
+      return $(each).attr("id");
+    })
+    .get();
 
-  window.location.replace(
-    `/mealplan/${id}/add/results?day=${day}&meal=${meal}`
-  );
+  const intolerance = $("#intolerance-input")
+    .find(":checked")
+    .map((i, each) => {
+      return $(each).attr("id");
+    })
+    .get();
+
+  // window.location.replace(
+  //   `/mealplan/${id}/add/results?day=${day}&meal=${meal}`
+  // );
 };
 
 $("#meal-search").submit(handleSubmit);
