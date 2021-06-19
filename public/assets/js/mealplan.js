e851d2b661848f9f198947e2215ae62d493de1fc
Pamela <pamela_bhopal@hotmail.co.uk>
2021-06-19 11:46:03 +0100

    create handle click function

--- public/assets/js/mealplan.js
+++ public/assets/js/mealplan.js
@@ -1,9 +1,24 @@
-const handleClick = (event) => {
+const handleClick = async (event) => {
   event.preventDefault();
   const selectedButton = event.target;
   //Fix click on icon
   const day = $(selectedButton).attr("data-day");
   const meal = $(selectedButton).attr("data-meal");
-  console.log(meal);
+  const id = $(event.currentTarget).attr("id");
+
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+  };
+
+  const response = await fetch(
+    `/mealplan/${id}/add?day=${day}&meal=${meal}`,
+    options
+  );
+  console.log(response);
 };
-$("#mealPlanTable").click(handleClick);
+
+$('[name="mealPlanTable"]').click(handleClick);
