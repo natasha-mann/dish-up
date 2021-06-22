a5983a15fbc9cc97c1dc1cc025cf190e3a7e8761
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-22 11:13:19 +0100

    added delete button and view route

--- src/controllers/api/days.js
+++ src/controllers/api/days.js
@@ -46,67 +46,6 @@ const addDay = async (req, res) => {
   }
 };
 
-const updateDay = async (req, res) => {
-  try {
-    const {
-      dayId: id,
-      day: name,
-      meal,
-      mealId,
-      mealPlanId: meal_plan_id,
-    } = req.body;
-
-    if (meal === "breakfast") {
-      const updatedDay = await Day.update(
-        {
-          name,
-          breakfast_meal_id: mealId,
-          meal_plan_id,
-        },
-        { where: { id } }
-      );
-
-      if (!updatedDay) {
-        return res.status(404).json({ error: "unable to update day" });
-      }
-      return res.status(200).json(updatedDay);
-    }
-
-    if (meal === "lunch") {
-      const updatedDay = await Day.update(
-        {
-          name,
-          lunch_meal_id: mealId,
-          meal_plan_id,
-        },
-        { where: { id } }
-      );
-
-      if (!updatedDay) {
-        return res.status(404).json({ error: "unable to update day" });
-      }
-      return res.status(200).json(updatedDay);
-    }
-
-    if (meal === "dinner") {
-      const updatedDay = await Day.update(
-        {
-          name,
-          dinner_meal_id: mealId,
-          meal_plan_id,
-        },
-        { where: { id } }
-      );
-
-      if (!updatedDay) {
-        return res.status(404).json({ error: "unable to update day" });
-      }
-      return res.status(200).json(updatedDay);
-    }
-  } catch (error) {
-    console.error(`${error.message}`);
-    return res.status(500).json({ error: "failed to add day" });
-  }
-};
+const updateDay = async (req, res) => {};
 
 module.exports = { addDay, updateDay };
