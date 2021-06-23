996b04cfc79a492bdd372a45db7b70e14a851a03
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-06-23 21:53:03 +0100

    heroku changes

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,16 +1,24 @@
 require("dotenv").config();
 
-const Sequelize = require("sequelize");
+const sequelize = require("sequelize");
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "localhost",
-      dialect: "mysql",
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-      logging: false,
-    });
+const dbOptions = {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  port: process.env.DB_PORT,
+  logging: false,
+};
 
-module.exports = sequelize;
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = new sequelize(process.env.JAWSDB_URL);
+} else {
+  connection = new sequelize(dbName, dbUser, dbPassword, dbOptions);
+}
+
+module.exports = connection;
